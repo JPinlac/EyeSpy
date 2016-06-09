ea9023924e5baa2a64abb23c813cbf4aa806f595
Jonathan Pinlac <jonppinlac@gmail.com>
2016-06-09 10:11:30 -0400

    Added constraints and user class

---
+++ EyeSpy/EyeSpy/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  EyeSpy
+//
+//  Created by DetroitLabs on 6/9/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
