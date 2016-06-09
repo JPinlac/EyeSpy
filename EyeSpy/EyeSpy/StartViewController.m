0fc47f376ad731e5f16275f02953781fd483de94
Michael Jasinski <michael.jasinski@detroitlabs.com>
2016-06-09 09:02:17 -0400

    Add pictures basic logic on game VC

--- EyeSpy/EyeSpy/StartViewController.m
+++ EyeSpy/EyeSpy/StartViewController.m
@@ -9,20 +9,42 @@
 #import "StartViewController.h"
 
 @interface StartViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *game1;
 
+@property (weak, nonatomic) IBOutlet UIImageView *game2;
 @end
 
 @implementation StartViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+//    UITapGestureRecognizer *tapImage1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(image1Tapped:)];
+//    UITapGestureRecognizer *tapImage2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(image2Tapped:)];
+//    
+//    [_game1 addGestureRecognizer:tapImage1];
+//    [_game1 addGestureRecognizer:tapImage2];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+//Tap function setup for selecting game
+//-(void)image1Tapped:(UIGestureRecognizer *)tap{
+// 
+//    _selectedQuestion = @"Turtle";
+//    [self performSegueWithIdentifier:@"questionSeg" sender:self];
+//}
+//
+//
+//-(void)image2Tapped:(UIGestureRecognizer *)tap{
+//    
+//    _selectedQuestion = @"Turtle";
+//    [self performSegueWithIdentifier:@"questionSeg" sender:self];
+//}
+
 
 /*
 #pragma mark - Navigation
